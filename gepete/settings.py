ae89d9ca578d1e19fb1c3c87ecede3779aa27112
Eric Baptista <Eric Baptista>
2023-07-26 00:40:52 +0200

    allow host

--- gepete/settings.py
+++ gepete/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ez8t8#ba)(cc8xcho%d4ajowm660t+&gnksoy**rcyw*s8nm4o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

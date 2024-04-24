1a779cb99f90e113f9136f05651b60146b5ca554
Eric Baptista <eric.macedob@gmail.com>
2024-04-24 18:29:28 -0300

    redirect base to app

--- gepete/urls.py
+++ gepete/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('/app')),
     path("app/", include("app.urls")),
     path("admin/", admin.site.urls),
 ]

e3e67f0e539c930caab2912200317bf01cc3a6ea
Eric Baptista <eric.macedob@gmail.com>
2024-04-25 00:10:32 -0300

    cleanup and pylint

--- app/chatgpt/chatgpt.py
+++ app/chatgpt/chatgpt.py
@@ -8,35 +8,32 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 
 def chat_complete(prompt):
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", 
+        model="gpt-3.5-turbo",
         messages=[
             {"role": "user", "content": f"{prompt}"},
         ],
-        temperature=0.7,  
-        max_tokens=1000  
+        temperature=0.7,
+        max_tokens=1000
     )
 
     return response
 
 def img_gen(prompt):
     response = openai.Image.create(
-        model="dall-e-3",  
-        prompt=f"{prompt}",  
-        n=1,  
+        model="dall-e-3",
+        prompt=f"{prompt}",
+        n=1,
         size="1024x1024",
     )
 
     return response
 
-def aud2text(f, lang):
+def aud2text(aud_file, lang):
     response = openai.Audio.transcribe(
-        file = f,
+        file = aud_file,
         model='whisper-1',
         temperature = 0.7,
         language = lang
     )
 
     return response
-
-# heitor/gouveia: aqui precisa de uma function p enviar um arquivo pra api da OpenAI
-# similar as func acima
--- app/views.py
+++ app/views.py
@@ -1,12 +1,5 @@
-from django.http import HttpResponse
-from django.template import loader
 from django.shortcuts import render
-from django.core.files.base import ContentFile
-import asyncio
-
-from django.core.files.storage import default_storage
 from .chatgpt import chatgpt
-import openai
 
 def index(request):
     return render(request,'app/index.html')
@@ -15,42 +8,39 @@ def img(request):
     if request.method == 'POST':
         prompt = request.POST.get('prompt')
         if prompt:
-            r = chatgpt.img_gen(prompt)
-            response = r['data'][0]['url']
+            res = chatgpt.img_gen(prompt)
+            response = res['data'][0]['url']
             context = {'response' : response}
             return render(request, 'app/img.html', context)
-        else:
-            context = {'response' : None}
-            return render(request,'app/img.html', None)
+        context = {'response' : None}
+        return render(request,'app/img.html', None)
     context = {'response' : None}
     return render(request,'app/img.html', None)
 
 def aud(request):
     if request.method == 'POST':
-        file = request.FILES.get('mp3_file')
+        aud_file = request.FILES.get('mp3_file')
         lang = request.POST.get('lang')
-        if file:
-            r = chatgpt.aud2text(file, lang)
-            response = r["text"]
+        if aud_file:
+            res = chatgpt.aud2text(aud_file, lang)
+            response = res["text"]
             context = {'response' : response}
             return render(request, 'app/aud.html', context)
-        else:
-            return render(request,'app/aud.html')
+        return render(request,'app/aud.html')
     return render(request,'app/aud.html')
-        
+
 
 def chat(request):
     if request.method == 'POST':
         prompt = request.POST.get('prompt')
         if prompt:
-            r = chatgpt.chat_complete(prompt)
-            response = r['choices'][0]['message']['content']
+            res = chatgpt.chat_complete(prompt)
+            response = res['choices'][0]['message']['content']
             context = {'response' : response}
             return render(request, 'app/chat.html', context)
-        else:
-            return render(request,'app/chat.html')
+        return render(request,'app/chat.html')
     return render(request,'app/chat.html')
 
-def file(request):
-    #heitor/gouveia: isso aqui precisa ter uma logica de rede (HTTP protocol) similar as outras funcoes 
-    return render(request,'app/file.html')
+def file():
+    # implementation pending
+    pass
